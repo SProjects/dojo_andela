f34acdb25588f0944479c8e38f95ee4c063bd104
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-25 12:27:07 +0300

    Adds tests and functionality to create and add office objects to the dojo.

--- app/models/dojo.py
+++ app/models/dojo.py
@@ -1,5 +1,23 @@
+from room import Office
+
+
 class Dojo(object):
+    OFFICE_ROOM_TYPE = 'OFFICE'
+
     def __init__(self, name, location):
         self.name = name
         self.location = location
+        self.offices = []
+
+    def create_room(self, room_names, room_type):
+        if room_type == self.OFFICE_ROOM_TYPE:
+            self._add_offices(room_names)
+
+    def _add_offices(self, office_names):
+        for name in office_names:
+            office = Office(name)
+            self.offices.append(office)
+
+
+
 
--- test/models/TestDojo.py
+++ test/models/TestDojo.py
@@ -1,12 +1,20 @@
 import unittest
 from app.models.dojo import Dojo
+from app.models.room import Office
 
 
 class TestDojo(unittest.TestCase):
     def setUp(self):
         self.dojo = Dojo('Andela Dojo', 'Nairobi')
+        self.office_room_type = 'OFFICE'
 
     def test_dojo_responds_to_its_properties(self):
         self.assertListEqual([self.dojo.name, self.dojo.location], ['Andela Dojo', 'Nairobi'])
 
+    def test_create_room_adds_new_office_object_to_dojo(self):
+        self.dojo.create_room(['office_name'], self.office_room_type)
+        self.assertEqual(self.dojo.offices, [Office('office_name')])
 
+    def test_create_room_adds_multiple_offices_objects_to_do(self):
+        self.dojo.create_room(['office1', 'office2'], self.office_room_type)
+        self.assertListEqual(self.dojo.offices, [Office('office1'), Office('office2')])

c2e2dc246ed4631fe0501d05a721726dbc2e9008
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-27 21:04:26 +0300

    Removes echoing.

--- app/db/models.py
+++ app/db/models.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-engine = create_engine("sqlite:///andela_dojo.db", echo=True)
+engine = create_engine("sqlite:///andela_dojo.db", echo=False)
 Base = declarative_base()
 
 eat_timezone = pytz.timezone('Africa/Nairobi')

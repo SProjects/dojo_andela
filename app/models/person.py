1d9c5d7c5f220e68ecec425af0cd845178350108
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-27 20:10:48 +0300

    Adds functionality to save people to the database.

--- app/models/person.py
+++ app/models/person.py
@@ -1,3 +1,8 @@
+from sqlalchemy.orm import sessionmaker
+from app.db.models import Fellow as DBFellow, Staff as DBStaff, engine
+from app.db.models import Office as DBOffice, Livingspace as DBLivingspace
+
+
 class Person(object):
     def __init__(self, name, office):
         self.name = name
@@ -5,6 +10,14 @@ class Person(object):
 
     def __eq__(self, other):
         return self.name == other.name
+
+    def get_office_from_db(self, session, office):
+        with session.no_autoflush:
+            return session.query(DBOffice).filter_by(name=office.name).first()
+
+    def get_livingspace_from_db(self, session, livingspace):
+        with session.no_autoflush:
+            return session.query(DBLivingspace).filter_by(name=livingspace.name).first()
 
 
 class Fellow(Person):
@@ -16,8 +29,38 @@ class Fellow(Person):
     def wants_accommodation(self):
         return self.accommodation == 'Y'
 
+    @staticmethod
+    def save(dojo):
+        session = dojo.session
+        with session.no_autoflush:
+            in_memory_fellows = dojo.fellows
+            for in_memory_fellow in in_memory_fellows:
+                in_memory_office = in_memory_fellow.office
+                in_memory_livingspace = in_memory_fellow.livingspace
+
+                office = in_memory_fellow.get_office_from_db(session, in_memory_office) if in_memory_office else None
+                livingspace = in_memory_fellow.\
+                    get_livingspace_from_db(session,in_memory_livingspace) if in_memory_livingspace else None
+
+                db_fellow = DBFellow(in_memory_fellow.name, in_memory_fellow.accommodation)
+                db_fellow.office = office
+                db_fellow.livingspace = livingspace
+                session.add(db_fellow)
+
 
 class Staff(Person):
     def __init__(self, name):
         super(Staff, self).__init__(name, None)
 
+    @staticmethod
+    def save(dojo):
+        session = dojo.session
+        with session.no_autoflush:
+            in_memory_staff = dojo.staff
+            for in_memory_staff in in_memory_staff:
+                in_memory_office = in_memory_staff.office
+                office = in_memory_staff.get_office_from_db(session, in_memory_office) if in_memory_office else None
+
+                db_staff = DBStaff(in_memory_staff.name)
+                db_staff.office = office
+                session.add(db_staff)

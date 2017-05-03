42f6f8750f376fead5984e332138a78ddd15b514
Sebuuma Daniel <sedzsoft@gmail.com>
2017-05-04 01:39:59 +0300

    Refactors _unallocated_fellows() to return better results for fellows that want accommodation but were earlier unallocated.

--- app/models/dojo.py
+++ app/models/dojo.py
@@ -176,7 +176,8 @@ class Dojo(object):
             print ", ".join([staff.name.upper() for staff in unallocated_staff])
 
     def _unallocated_fellows(self):
-        return [fellow for fellow in self.fellows if not fellow.office or not fellow.livingspace]
+        return [fellow for fellow in self.fellows if
+                not fellow.office or (not fellow.livingspace and fellow.wants_accommodation)]
 
     def _unallocated_staff(self):
         return [staff for staff in self.staff if not staff.office]
